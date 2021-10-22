fd818dc891ebf484f7b474faf6941c69a8ff9443
LMR <3524613297@qq.com>
2021-10-22 22:47:24 +0800

    21--10-22更新

--- 21-10-21/21-10-21/main.cpp
+++ 21-10-21/21-10-21/main.cpp
@@ -1,8 +1,8 @@
 #include  "class.h"
 int main()
 {
-	char str[] = "abcd1234";
-	//char *str = "abcd1234";
+	//char str[] = "abcd1234";
+	char *str = "abcd1234";//该字符串“abcd1234",不能被修改！！！
 
 	printf("%s\n", str);
 	ReverseString(str);
