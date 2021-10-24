92220d1b3e93dfbc30a6dbfc6012008cbefcc69e
LMR <3524613297@qq.com>
2021-10-24 21:39:36 +0800

    1

--- 21-10-23/21-10-23/main.cpp
+++ 21-10-23/21-10-23/main.cpp
@@ -6,6 +6,8 @@ int main()
 	//char[]
 	//被当成普通的char数组
 	//被整体当成字符串使用
+	//数组的空间是在对应函数的栈帧内部开辟的，换言之，是在栈上开辟空间发（常规情况）
+
 	char arr[M] = { 'a', 'b', 'c','\0' };
 	int num = sizeof(arr) / sizeof(arr[0]);
 
